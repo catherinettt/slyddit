b21068786a681fdb98c6c1d21a9e397d188718b8
Dan Ho <dth4@sfu.ca>
2012-10-17 21:59:23 -0700

    add filter

--- routes/routes.js
+++ routes/routes.js
@@ -4,20 +4,27 @@ var mime = require('mime-magic');
 
 exports.index = function (req, res) {
 	var result = {};
-	request("http://www.reddit.com/hot.json", function(error, resp, body){
+	request("http://www.reddit.com/hot.json", function(error, resp, body)
+	{
+	if(!error && resp.statusCode == 200)
+	{
 		body = JSON.parse(body);
 		var list = body["data"]["children"];
 		list.forEach(function(item, i){
 			var url = item["data"]["url"];
-				console.log(type);
-			})
+			var ext = url.substr(-4);
+			if(ext == '.gif' || ext == '.png' || ext == '.jpg')
+			{
+				result[i] = item["data"];
+				console.log(result[i]["url"]);
+			}
 		})
-		//console.log(list);
+	        res.render("index", {
+                                        title:"Slyddit", result: result
+                                }, function (err, rendered) {
+                                        res.writeHead(200, {'Content-Type':'text/html'});
+                                        res.end(rendered);
+                                        })
+	}	
 	})
-	res.render("index", {
-					title:"Hello World",
-				}, function (err, rendered) {
-					res.writeHead(200, {'Content-Type':'text/html'});
-					res.end(rendered);
-					})
-					}
+}
